cbd0afd7a266172c307eb09a96a316f74de9cc9e
sagor1155 <sagor.eee.cuet@gmail.com>
2020-03-12 17:01:39 +0600

    use of prototype for class implementation in js

--- objects.js
+++ objects.js
@@ -13,34 +13,63 @@
 // console.log(userOne.name);
 // console.log(userOne.email);
 
-class User{
-    constructor(name, email){
-        this.name = name;
-        this.email = email;
-        this.score = 0;
-    }
+// class User{
+//     constructor(name, email){
+//         this.name = name;
+//         this.email = email;
+//         this.score = 0;
+//     }
 
-    login(){
-        console.log(this.email, "has logged in");
-        return this;
-    }
+//     login(){
+//         console.log(this.email, "has logged in");
+//         return this;
+//     }
 
-    logout(){
-        console.log(this.email, "has logged out");
-        return this;
-    }
+//     logout(){
+//         console.log(this.email, "has logged out");
+//         return this;
+//     }
 
-    updateScore(){
-        this.score++;
-        console.log(this.name + " has score of " + this.score);
-        return this;
-    }
+//     updateScore(){
+//         this.score++;
+//         console.log(this.name + " has score of " + this.score);
+//         return this;
+//     }
+// }
+
+// class Admin extends User{
+//     deleteUser(user){
+//         users = users.filter(u => {
+//             return u.email != user.email;
+//         })
+//     }
+// }
+
+function User(name, email){
+    this.name = name;
+    this.email = email;
+    this.online = false;
+}
+
+User.prototype.login = function(){
+    this.online = true;
+    console.log(this.email, "has just logged in");
+    return this;
+}
+
+User.prototype.logout = function(){
+    this.online = false;
+    console.log(this.email, "has just logged out");
+    return this;
 }
 
 var userOne = new User("rhyno", "rhyno@gmail.com");
 var userTwo = new User("groot", "groot@gmail.com");
+// var admin   = new Admin("dstrange", "dstrange@gmail.com");
+// var users = [userOne, userTwo, admin];
+// admin.deleteUser(userOne);
 
 // method chaining 
-userOne.login().updateScore().updateScore().logout();
-userTwo.login().updateScore().updateScore().logout();
+// userOne.login().updateScore().updateScore().logout();
+// userTwo.login().updateScore().updateScore().logout();
 
